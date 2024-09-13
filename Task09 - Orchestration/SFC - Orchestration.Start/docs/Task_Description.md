8e3fe3e141436485708c173ce933431f262eb88c
mr0zek <szymon.szylhabel@gmail.com>
2024-09-13 10:47:15 +0200

    Choreography

---
+++ Task09 - Orchestration/SFC - Orchestration.Start/docs/Task_Description.md
@@ -0,0 +1,12 @@
+# Ćwiczenie: Orkiestracja
+
+Za pomocą orkiestracji zaimplementuj proces zakładania konta użytkownika
+
+## Rejestracja użytkownika:
+Aplikacja pozwala na zarejestrowanie konta użytkownika.
+
+Po wypełnieniu formularza do użytkownika zostaje wysłany email z linkiem potwierdzającym.
+Po zarejestrowaniu zakładany jest alarm na adres który użytkownik poda w formularzu rejestracyjnym.
+Formularz ten zawiera również domyślny sposób komunikacji z użytkownikiem - Email/Sms.
+
+Test - UserApi.UserApiTests.AccountCreationSuccessScenario
---
+++ Task09 - Orchestration/SFC - Orchestration.Start/src/SFC.Processes/Features/UserRegistration/UserRegistrationHandler.cs
@@ -0,0 +1,24 @@
+﻿using Automatonymous;
+using SFC.Accounts.Features.CreateAccount.Contract;
+using SFC.Accounts.Features.GetAccountByLoginName;
+using SFC.Alerts.Features.RegisterAlertCondition.Contract;
+using SFC.Infrastructure.Interfaces;
+using SFC.Notifications.Features.SendNotification.Contract;
+using SFC.Notifications.Features.SetNotificationEmail.Contract;
+using SFC.Processes.Features.UserRegistration.Contract;
+using System;
+
+namespace SFC.Processes.Features.UserRegistration
+{
+  class UserRegistrationHandler // implement handlers
+  {
+    private readonly ICommandBus _commandBus;
+    private readonly IQuery _query;
+    
+    public UserRegistrationHandler(ICommandBus commandBus, IQuery query)
+    {
+      _commandBus = commandBus;
+      _query = query;
+    }    
+  }
+}
